5d547c1f525906f87a3f082340226eec42bf368c
Gerardo Lara <124016989+GerardoLaraGonzalez@users.noreply.github.com>
2023-11-13 13:08:21 -0600

    Add files via upload

--- Pagina/conexion.js
+++ Pagina/conexion.js
@@ -1,21 +1,21 @@
-let url = 'http://localhost:5188/api/Libro/LibroAutor';
+let url = "http://localhost:5188/api/Libro/LibroAutor";
 fetch(url)
-    .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch(error => console.log(error))
+  .then((response) => response.json())
+  .then((data) => mostrarData(data))
+  .catch((error) => console.log(error));
 
 const mostrarData = (data) => {
-  console.log(data)
-  let body = ''
-  for(let i = 0; i < data.length; i++){
+  console.log(data);
+  let body = "";
+  for (let i = 0; i < data.length; i++) {
     body += `<tr>
-                 <td>${data[i].nombreAutor}</td>
                  <td>${data[i].nombreLibro}</td>
+                 <td>${data[i].nombreAutor}</td>
                  <td>${data[i].chapters}</td>
                  <td>${data[i].pages}</td>
                  <td>$${data[i].price}</td>
-                 </tr>`
+                 </tr>`;
   }
 
-  document.getElementById('data').innerHTML = body
-}
+  document.getElementById("data").innerHTML = body;
+};
